7212da30f38e5c71b2447858fffec172d815a3c9
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-03-07 06:35:19 +0530

    Convert events to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+const dotenv = require("dotenv")
+dotenv.config()
+
+const ormOptions: TypeOrmModuleOptions = {
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormOptions),
+    EventsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
