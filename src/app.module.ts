e63e5219b58459747a20d39e5dce85851b293ecb
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-03-11 08:48:17 +0530

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,27 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
-const dotenv = require("dotenv")
-dotenv.config()
 
-const ormOptions: TypeOrmModuleOptions = {
-  type: "mysql",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-};
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(ormOptions),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [Event],
+      synchronize: true,
+    }),
     EventsModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
