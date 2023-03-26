80642f109d07f3e479c2d6b7c0b833878b2fbe4f
Tamilmozhi Gunasekar <tgunasekar@rbbn.com>
2023-03-26 08:15:24 +0530

    Add basic e2e for events

--- test/events.e2e-spec.ts
+++ test/events.e2e-spec.ts
@@ -1,10 +1,28 @@
 import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
-import { AppModule } from "./../src/app.module";
+import * as fs from 'fs';
+import * as path from 'path';
 import * as request from 'supertest';
+import { Connection, DataSource } from "typeorm";
+import { AppModule } from "./../src/app.module";
 
 describe('Events (e2e)', () => {
   let app: INestApplication;
+  let datasource: DataSource;
+  let connection: Connection;
+
+  const loadFixtures = async (sqlFilename: string) => {
+    const sql = fs.readFileSync(path.join(__dirname, 'fixtures', sqlFilename), 'utf8');
+
+    // const queryRunner = datasource.createQueryRunner('master');
+    const queryRunner = connection.driver.createQueryRunner('master');
+
+    for (const statement of sql.split(';')) {
+      if (statement.trim()) {
+        await queryRunner.query(statement);
+      }
+    }
+  }
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,6 +31,9 @@ describe('Events (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    datasource = app.get(DataSource);
+    connection = app.get(Connection);
   });
 
   // Make sure to close the app after all tests are done
@@ -21,9 +42,20 @@ describe('Events (e2e)', () => {
   });
 
 
-  it('/events (GET)', async () => {
+  it('should return empty list of events', async () => {
     return request(app.getHttpServer())
       .get('/events')
+      .expect(200)
+      .then(response => {
+        expect(response.body.data.length).toBe(0);
+      });
+  });
+
+  it('should return a single event', async () => {
+    await loadFixtures('1-event-1-user.sql');
+
+    return request(app.getHttpServer())
+      .get('/events/1')
       .expect(200);
   });
 });
